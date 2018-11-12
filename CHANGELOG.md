1dd7d19531077a3a9178acb5a1bee7b18a3bc14d
Valerii Savchenko <v.savchenko@cpcs.ws>
2018-11-12 17:42:16 +0200

    Update CHANGELOG.md (v0.1.5)

--- CHANGELOG.md
+++ CHANGELOG.md
@@ -1,4 +1,16 @@
 # CHANGELOG
 
+## v0.1.5 (2018-11-12)
+
+### New
+
+### Change
+
+### Delete
+
+---
+
+# CHANGELOG
+
 ## v0.1.4 (2018-11-12)
 
--- build_git_tags.sh
+++ build_git_tags.sh
@@ -1,9 +1,9 @@
 #!/usr/bin/env bash
 
 set -u
-#set -x
+set -x
 
-VERSION_GIT_TAG=18.06
+VERSION_GIT_TAG="18.06"
 
 git tag -d "$VERSION_GIT_TAG"
 git tag -am "$VERSION_GIT_TAG" "$VERSION_GIT_TAG"
