e59da8539d7e600caabb8ae6dcf8bdfc74105a86
Valerii Savchenko <v.savchenko@cpcs.ws>
2018-11-12 17:41:53 +0200

    Update CHANGELOG.md (v0.1.4)

--- build_git_tags.sh
+++ build_git_tags.sh
@@ -7,5 +7,5 @@ VERSION_GIT_TAG=18.06
 
 git tag -d "$VERSION_GIT_TAG"
 git tag -am "$VERSION_GIT_TAG" "$VERSION_GIT_TAG"
-git push --delete "$VERSION_GIT_TAG"
+git push --delete origin
 git push --tags origin
